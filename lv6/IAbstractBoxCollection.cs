07a27fb51227b26bbbd8935cb79ef8686201eb00
Anto Ivic <anto.ivic23@gmail.com>
2020-05-20 18:25:37 +0200

    Zadatak2

---
+++ lv6/IAbstractBoxCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lv6
+{
+    interface IAbstractBoxCollection 
+    {
+
+        IAbstractBoxIterator GetIterator();
+    }
+}
